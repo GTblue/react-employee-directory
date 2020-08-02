8abd78265bb501d65b1f94ee57c689844ab25185
Gregory Monroe II <gregthemusician@gmail.com>
2020-08-02 17:26:52 -0400

    Updates

---
+++ precache-manifest.df6f6f32b341b4b86141fd9b5cea87d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77cf3cd14e0ebe81f676c8f47f9b47a2",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "f65a76fb5a32acb85aca",
+    "url": "/react-employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9b93b183d9c1e16f769",
+    "url": "/react-employee-directory/static/js/2.19f9d7d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.19f9d7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65a76fb5a32acb85aca",
+    "url": "/react-employee-directory/static/js/main.62232a32.chunk.js"
+  },
+  {
+    "revision": "097cd2055010e01755e3",
+    "url": "/react-employee-directory/static/js/runtime-main.6936f29a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-employee-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
