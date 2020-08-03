396000ecd4e10fc3411625a1e41cc27ece22824d
Gregory Monroe II <gregthemusician@gmail.com>
2020-08-03 15:55:20 -0400

    fixed componentDidMount section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,16 @@ class Search extends Component {
   // When the component mounts, get a list of all available base breeds and update this.state.breeds
   componentDidMount() {
     axios
-
+      .get("https://randomuser.me/api/?results=100")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          results: response.data.message,
+        });
+      })
+      .catch((err) => {
+        console.log(err)
+      });
   }
 
   handleInputChange = event => {
